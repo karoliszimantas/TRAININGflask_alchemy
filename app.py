e59cdcb5d65706a035a7cd6bb5e6d23c91252a9d
Karolis <zimantask@gmail.com>
2021-01-07 19:35:25 +0200

    add

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from faker import Faker
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 
 fake = Faker()
@@ -23,15 +23,15 @@ def add_orders():
     for _ in range(1000):
 
         customer = random.choice(customers)
-        ordered_date=fake.date_time_this_year(),
-        shipped_date=random.choices([None, fake.date_time_between(start_date=ordered_date)], [10,90])[0]
+        ordered_date = fake.date_time_this_year()
+        shipped_date = random.choices([None, fake.date_time_between(start_date=ordered_date)], [10, 90])[0]
         delivered_date=None,
         if shipped_date:
-            delivered_date = random.choices([None, fake.date_time_between(start_date=shipped_date)], [50,50][0])
-        coupon_code = random.choices([None, "Premium", "Disqount"],[70,20,10])[0]
+            delivered_date = random.choices([None, fake.date_time_between(start_date=shipped_date)])
+        coupon_code = random.choices([None, "Premium", "Discount"],[70,20,10])[0]
 
         order = Order(
-            customer_id=customer_id,
+            customer_id=customer.id,
             order_date=ordered_date,
             shipped_date=shipped_date,
             coupon_code=coupon_code
@@ -66,6 +66,26 @@ def create_random_data():
     add_products()
     add_order_products()
 
+def orders_by(customer_id=1):
+    customer_orders = Order.query.filter_by(customer_id=customer_id).all()
+    for order in customer_orders:
+        print(order.customer.first_name)
+
+def pending_orders(customer_id=1):
+    pending_orders = Order.query.filter(Order.shipped_date.is_(None)).all()
+    for order in pending_orders:
+        print(order.order_date)
+
+def customer_count():
+    print(Customer.query.count())
+
+def orders_with_code():
+    orders = Order.query.filter(Order.coupon_code.isnot(None)).all()
+    for order in orders:
+        print(order.coupon_code)
+
+def revenue(x_days=30):
+    print(db.session.query(db.func.sum(Product.price)).join(order_product).join(Order).filter(Order.order_date > (datetime.now() - timedelta(days=x_days))).all())
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
